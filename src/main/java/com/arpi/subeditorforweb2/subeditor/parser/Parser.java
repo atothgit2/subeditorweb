03cde918d68aba8938f5869f26b4028b9785ae49
Tóth Árpád <toth.arpad@electool.intra>
2021-11-25 23:17:46 +0100

    new srt file is now generated using frontend form data, testing needid, next step: return file to frontend

--- src/main/java/com/arpi/subeditorforweb2/subeditor/parser/Parser.java
+++ src/main/java/com/arpi/subeditorforweb2/subeditor/parser/Parser.java
@@ -1,86 +1,78 @@
 package com.arpi.subeditorforweb2.subeditor.parser;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-    public List<SubEntry> readAndParse (String fileLocation) {
-        ArrayList<String> originalContentInStrings = readFileAndConvertSRT(fileLocation);
-        List<SubEntry> result = linesToObjects(originalContentInStrings);
+  public List<SubEntry> readAndParse (String fileContent) {
+      ArrayList<String> originalContentInStrings = readFileAndConvertSRT(fileContent);
+      List<SubEntry> result = linesToObjects(originalContentInStrings);
 
-        return result;
+      return result;
+  }
+
+  private ArrayList<String> readFileAndConvertSRT(String originalFileContent) {                 // "subs/mysub.srt"
+    ArrayList<String> linesInStrings = new ArrayList<String>();
+    String content = originalFileContent;
+
+    String[] readContents = originalFileContent.split("\\r?\\n", 0);
+
+    // Remove \ufeff tag from beginning of first string
+    // https://stackoverflow.com/questions/3255993/how-do-i-remove-%C3%AF-from-the-beginning-of-a-file
+    Pattern ufeffPattern = Pattern.compile("\\ufeff[0-9]+");
+
+    // elég a legelső sort megvizsgálni a readContentsben, nem kell minden soron végig menni
+    // a linesInStrings-be ne rakjuk be az első sort, ha BOM-es element van benne ('for' nem kell)
+
+    for (String str : readContents) {
+      Matcher ufeffMatcher = ufeffPattern.matcher((String) str);
+      boolean isItUfeff = ufeffMatcher.matches();
+
+    if (isItUfeff) {
+      linesInStrings.add(str.replaceAll("\\ufeff",""));
+      continue;
+    } else {
+      linesInStrings.add(str);
     }
+  }
+    return linesInStrings;
+  }
 
-    private ArrayList<String> readFileAndConvertSRT(String originalFileLocation) {                 // "subs/mysub.srt"
-        Path fileName = Path.of(String.valueOf(originalFileLocation));
-        ArrayList<String> linesInStrings = new ArrayList<String>();
+  // erre unit teszteket csinálni később
+  public List<SubEntry> linesToObjects(ArrayList<String> contentStrings) {
+    SubEntry currentSubEntry = new SubEntry();
+    List<SubEntry> results = new ArrayList<SubEntry>();
 
-        try {
-            String readContent = Files.readString(fileName);
-            String[] readContents = readContent.split("\\r?\\n", 0);
+    Pattern positionPattern = Pattern.compile("\\A\\d+$");
+    Pattern timingPattern = Pattern.compile("\\d{2}:\\d{2}:\\d{2},\\d{3}.-->.\\d{2}:\\d{2}:\\d{2},\\d{3}");
 
-            // Remove \ufeff tag from beginning of first string
-            // https://stackoverflow.com/questions/3255993/how-do-i-remove-%C3%AF-from-the-beginning-of-a-file
-            Pattern ufeffPattern = Pattern.compile("\\ufeff[0-9]+");
+    for (int i = 0; i < contentStrings.size(); i++) {
+      Matcher timingMatcher = timingPattern.matcher((String) contentStrings.get(i));
+      Matcher positionMatcher = positionPattern.matcher((String) contentStrings.get(i));
 
-            // elég a legelső sort megvizsgálni a readContentsben, nem kell minden soron végig menni
-            // a linesInStrings-be ne rakjuk be az első sort, ha BOM-es element van benne ('for' nem kell)
+      boolean isItPosition = positionMatcher.matches();
+      boolean isItStamp = timingMatcher.matches();
 
-            for (String str : readContents) {
-                Matcher ufeffMatcher = ufeffPattern.matcher((String) str);
-                boolean isItUfeff = ufeffMatcher.matches();
-
-               if (isItUfeff) {
-                   linesInStrings.add(str.replaceAll("\\ufeff",""));
-                   continue;
-               } else {
-                   linesInStrings.add(str);
-               }
-           }
-        } catch (IOException e) { // file írás vagy olvasás az kötelezően kezelendő, később fixáljuk
-            e.printStackTrace();
+      if (currentSubEntry.getIndex() == null || currentSubEntry.getTiming() == null || currentSubEntry.getText() == "") {
+        if (isItPosition) {
+          currentSubEntry.setIndex((String) contentStrings.get(i));
+        } else if (isItStamp) {
+          currentSubEntry.setTiming((String) contentStrings.get(i));
+        } else if (!isItPosition && !isItStamp && !contentStrings.get(i).toString().isEmpty()) {
+          currentSubEntry.setTextConcat((String) contentStrings.get(i));
         }
-        return linesInStrings;
-    }
-
-    // erre unit teszteket csinálni később
-    public List<SubEntry> linesToObjects(ArrayList<String> contentStrings) {
-        SubEntry currentSubEntry = new SubEntry();
-        List<SubEntry> results = new ArrayList<SubEntry>();
-
-        Pattern positionPattern = Pattern.compile("\\A\\d+$");
-        Pattern timingPattern = Pattern.compile("\\d{2}:\\d{2}:\\d{2},\\d{3}.-->.\\d{2}:\\d{2}:\\d{2},\\d{3}");
-
-        for (int i = 0; i < contentStrings.size(); i++) {
-            Matcher timingMatcher = timingPattern.matcher((String) contentStrings.get(i));
-            Matcher positionMatcher = positionPattern.matcher((String) contentStrings.get(i));
-
-            boolean isItPosition = positionMatcher.matches();
-            boolean isItStamp = timingMatcher.matches();
-
-            if (currentSubEntry.getIndex() == null || currentSubEntry.getTiming() == null || currentSubEntry.getText() == "") {
-                if (isItPosition) {
-                    currentSubEntry.setIndex((String) contentStrings.get(i));
-                } else if (isItStamp) {
-                    currentSubEntry.setTiming((String) contentStrings.get(i));
-                } else if (!isItPosition && !isItStamp && !contentStrings.get(i).toString().isEmpty()) {
-                    currentSubEntry.setTextConcat((String) contentStrings.get(i));
-                }
-            }
-            else if (currentSubEntry.getIndex() != null && currentSubEntry.getTiming() != null && !isItPosition && !isItStamp && !contentStrings.get(i).toString().isEmpty()) {
-                currentSubEntry.setTextConcat((String) contentStrings.get(i));
-            }
-            else if (currentSubEntry.getIndex() != null && currentSubEntry.getTiming() != null && currentSubEntry.getText() != "") {
-                results.add(currentSubEntry);
-                currentSubEntry = new SubEntry();
-            }
-        }
+      }
+      else if (currentSubEntry.getIndex() != null && currentSubEntry.getTiming() != null && !isItPosition && !isItStamp && !contentStrings.get(i).toString().isEmpty()) {
+          currentSubEntry.setTextConcat((String) contentStrings.get(i));
+      }
+      else if (currentSubEntry.getIndex() != null && currentSubEntry.getTiming() != null && currentSubEntry.getText() != "") {
         results.add(currentSubEntry);
-        return results;
+        currentSubEntry = new SubEntry();
+      }
     }
+    results.add(currentSubEntry);
+    return results;
+  }
 }
--- src/main/java/com/arpi/subeditorforweb2/subeditorweb/TestController.java
+++ src/main/java/com/arpi/subeditorforweb2/subeditorweb/TestController.java
@@ -4,22 +4,61 @@ import com.arpi.subeditorforweb2.subeditor.fileExporter.FileExporter;
 import com.arpi.subeditorforweb2.subeditor.parser.*;
 import com.arpi.subeditorforweb2.subeditor.serializer.StringSerializer;
 import com.arpi.subeditorforweb2.subeditor.utils.FileUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.List;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class TestController {
   @PostMapping(value = "/testjava")
-  public void getData(@RequestParam("file") MultipartFile payload) {
-    System.out.println(payload);
+  // "newFileName", "adjustByTime", "delayByTime"
+  public void getData(
+      @RequestParam("file") MultipartFile file, 
+      @RequestParam("newFileName") String newFileName,
+      @RequestParam("adjustByTime") Double adjustByTime,
+      @RequestParam("delayByTime") Double delayByTime)
+  {
+    String fileContent = "";
 
+    try {
+      InputStream inputStream = file.getInputStream();
+      fileContent = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+      runSubEditor(newFileName, adjustByTime, delayByTime, fileContent);
+      //System.out.println(result);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+  }
+
+  public void runSubEditor(@RequestParam String newFileName, Double adjustByTime, Double delayByTime, String fileContent) {
     Parser parser = new Parser();
-    String fileLocation = "subs/mysub.srt";
+    // String fileLocation = "subs/mysub.srt";
 
     // Read file
-    List<SubEntry> subEntries = parser.readAndParse(fileLocation);
+    List<SubEntry> subEntries = parser.readAndParse(fileContent);
+//        String originalFileName = FileUtils.extractFilanameFromPath(fileLocation);
+
+    // Modify timestamps
+    TimingModifierService timingModifierService = new TimingModifierService();
+    List<SubEntry> modifiedSubentries = timingModifierService.modifySubentriesTiming(subEntries, adjustByTime);
+
+    // Modify duration
+    SubDisplayDurationModifierService subDisplayDurationModifierService = new SubDisplayDurationModifierService();
+    List<SubEntry> modifiedSubEntriesDuration = subDisplayDurationModifierService.modifyDisplayDuration(modifiedSubentries, delayByTime);
+
+    // Modified timestamps go to new string
+    StringSerializer stringSerializer = new StringSerializer();
+    String result = stringSerializer.convertToString(modifiedSubEntriesDuration);
+
+    // String to file
+    FileExporter exporter = new FileExporter();
+    Path fileLocationNew = FileUtils.fileNominator(newFileName);
+    exporter.exportStringToFile(result, fileLocationNew);
   }
 }
